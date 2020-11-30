00837dc7c234a07c7deb4c52e93149e0e3db337c
mesakh123 <cmesakh@ymail.com>
2020-11-30 22:47:44 +0800

    adding hand api

--- main/urls.py
+++ main/urls.py
@@ -6,7 +6,7 @@ from . import views
 app_name = "main"
 
 urlpatterns = [
-
     path("",views.index , name="index_url"),
+    path("handuploadapi/",views.handuploadapi ,name="handuploadapi_url"),
 
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,27 @@
 
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect,reverse
+from django.contrib import messages
+from demo.views_utils import *
 # Create your views here.
 def index(request):
     return render(request,"main/index.html",locals())
+
+def handuploadapi(request):
+    if request.method=="GET":
+        return render(request,"main/handupload.html",locals())
+    if len(request.POST.getlist("images")) is 0:
+        message="File can't be empty"
+        messages.error(request, message)
+        return render(request,"main/handupload.html",locals())
+    flag=True
+    try:
+        print("HERE")
+        hand64_sess,image_ids =  upload_process(request.POST.getlist("images"),"hand",[0])
+    except:
+        print("HERE2")
+        flag=False
+    if flag:
+        messages.success(request, 'upload success')
+    else:
+        messages.error(request,"upload fail")
+    return redirect(reverse("main:handuploadapi_url"))
