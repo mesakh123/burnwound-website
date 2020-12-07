187b456b3980fa66faab1ea735e7f46aeb22d65a
mesakh123 <cmesakh@ymail.com>
2020-12-07 18:08:37 +0800

    fixing add searchbar in admin

--- demo/admin.py
+++ demo/admin.py
@@ -6,7 +6,9 @@ from .models import PatientData,BurnDocument,HandDocument,PredictResult
 class PatientDataAdmin(admin.ModelAdmin):
     list_display = ["patient_id",'name','burn_type']
 
+class PredictResultAdmin(admin.ModelAdmin):
+    search_fields = ('result_code', )
 admin.site.register(PatientData,PatientDataAdmin)
 admin.site.register(BurnDocument)
 admin.site.register(HandDocument)
-admin.site.register(PredictResult)
+admin.site.register(PredictResult,PredictResultAdmin)
